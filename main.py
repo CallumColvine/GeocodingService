ba5f152f5d1ee255e812b5532a563a401884b286
Callum Colvine <callumcolvine@gmail.com>
2018-04-15 03:04:06 -0700

    Verified ability to call Google geocoding API

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import json
 ''' 
 ToDo:
 - Call Here geocoding services                          DONE
-- Call Google geocoding stuff                           IN PROGRESS
+- Call Google geocoding stuff                           DONE
 - Parse input arguments for command line interface      TODO
 - Enable RESTful calls on app                           TODO
 '''
@@ -19,10 +19,31 @@ ToDo:
 HERE_APP_ID = 'T8Yh97KaSNVLi0pA2AXi'
 HERE_APP_CODE = 'n0J58c0YsvFtUaX99I7UCQ'
 
+# Retrieved from https://developers.google.com/maps/documentation/geocoding/start
+GOOGLE_API_KEY = 'AIzaSyA-YjJR_ILnJurVXGpO2Rtk6ToE6P02djs'
+
 # From Google Maps
 MY_LAT = 49.287776
 MY_LON = -123.1361665
 MY_ADDRESS = '1655+Nelson+Street+Vancouver+Canada'
+
+
+def callGoogleGeocode():
+    lat = 0
+    lon = 0
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1655%20Nelson%20Street%2C%20Vancouver%2C%20Canada&key=AIzaSyA-YjJR_ILnJurVXGpO2Rtk6ToE6P02djs'
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        page = response.read()
+        # print (type(page))
+        page = page.decode('utf8').replace("'", '"')
+        page = json.loads(page)
+        print (page)
+        location = page.get("results", {})[0].get("geometry", {}).get("location")
+        lat = location.get("lat")
+        lon = location.get("lng")
+        assert abs(lat - MY_LAT) < 0.001 
+        assert abs(lon - MY_LON) < 0.001 
 
 
 def callHereGeocode():
@@ -51,6 +72,8 @@ def callHereGeocode():
 def main():
     print ("Hello there")
     callHereGeocode()
+    callGoogleGeocode()
+    print ("Sucessffully got lat/long from Here and Google")
 
 if __name__ == "__main__": 
     main()
